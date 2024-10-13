c60b07372d7d3482971fe881750e707029630963
辻響 <0131Hibiki-0131@users.noreply.github.com>
2024-10-14 01:26:20 +0900

    10/14 1:25　辻響　プレイヤーの左右移動　アニメーション実装

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 3.0f;
+    public float leftRightSpeed = 4.0f;
+    Rigidbody rb;
+    public Animator animator;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed);
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1);
+        }
+    }
+}
