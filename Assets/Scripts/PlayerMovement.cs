7806f32d23a34c7ce8e6753f56c44ebe4031d470
辻響 <0131Hibiki-0131@users.noreply.github.com>
2024-10-18 14:37:52 +0900

    10/18 14:35 辻響　アキオ導入。最初の３秒間カウントダウン時、画面に半透明の黒いスクリーンが覆い、左右移動が制限されるようになった。

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -9,23 +10,55 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody rb;
     public Animator animator;
 
+    private bool canMove = false;
+
+    public Image blackOverlay; 
+    public float delayBeforeHiding = 3.3f;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        Color color = blackOverlay.color;
+        color.a = 0.5f; 
+        blackOverlay.color = color;
+
+        blackOverlay.gameObject.SetActive(true);
+        StartCoroutine(EnableMovementAfterDelay(3.3f));
+        StartCoroutine(HideOverlayAfterDelay(delayBeforeHiding));
     }
 
     private void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
 
-        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        if (canMove)
         {
-            transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed);
-        }
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed);
+            }
 
-        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1);
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1);
+            }
         }
     }
+
+    private IEnumerator EnableMovementAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        canMove = true;
+    }
+
+    private IEnumerator HideOverlayAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Color color = blackOverlay.color; 
+        color.a = 0f; 
+        blackOverlay.color = color; 
+        blackOverlay.gameObject.SetActive(false); 
+    }
+
 }
