395a1ebc677a3d6e10c163cce69b128ff04c3075
辻響 <0131Hibiki-0131@users.noreply.github.com>
2024-10-23 20:12:21 +0900

    2024 20:12 辻響 Playerに重力をつけた

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("プレイヤー設定")]
     public float moveSpeed = 3.0f;
     public float leftRightSpeed = 4.0f;
+    public float gravity = -3f;
     Rigidbody rb;
     private Animator animator;
 
     private bool canMove = false;
     private bool isFalling = false;
     private bool isWalking = false;
+
 
     public Image blackOverlay; 
     public float delayBeforeHiding = 3.3f;
@@ -29,15 +33,19 @@ public class PlayerMovement : MonoBehaviour
         blackOverlay.gameObject.SetActive(true);
         StartCoroutine(EnableMovementAfterDelay(3.3f));
         StartCoroutine(HideOverlayAfterDelay(delayBeforeHiding));
+
+        Debug.Log(moveSpeed);
     }
 
     private void FixedUpdate()
     {
+        rb.AddForce(new Vector3(0f, gravity, 0f)); //= new Vector3(rb.velocity.x, rb.velocity.y * gravity * Time.fixedDeltaTime, rb.velocity.z);
+
         if (canMove)
         {
             // 前方移動
-            Vector3 moveDirection = transform.forward * moveSpeed;
-            rb.velocity = new Vector3(moveDirection.x, rb.velocity.y, moveDirection.z);
+            Vector3 moveDirection = transform.forward * moveSpeed ;
+            rb.velocity = new Vector3(moveDirection.x, rb.velocity.y, rb.velocity.z);
 
             // 左右移動
             if (isWalking)
