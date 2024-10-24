9d410d08020257b855fbfeac137ee47d67507b92
0yu-uy0 <e1c24059@oit.ac.jp>
2024-10-24 23:37:27 +0900

    10/24 23:36 中井優心 カウントダウン中のタイマーを消した

--- Assets/Scripts/TimeCounter.cs
+++ Assets/Scripts/TimeCounter.cs
@@ -8,19 +8,41 @@ public class TimeCounter : MonoBehaviour
 {
     //カウントダウン
     public float countdown = 60.00f;
+    public float countdownTime = 3f;
 
     //時間を表示するText型の変数
     public Text timeText;
 
+
+    //タイマー非表示
+    private bool countStart = false;
+
+    void Start()
+    {
+        StartCoroutine(StartCountdown());
+        timeText.enabled = false;
+
+    }
+
+    //3秒後にタイマースタート
+    IEnumerator StartCountdown()
+    {
+        yield return new WaitForSeconds(countdownTime);
+        countStart = true;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        //時間をカウントダウンする
-        countdown -= Time.deltaTime;
+        if (countStart)
+        {
+            timeText.enabled = true;
+            //時間をカウントダウンする
+            countdown -= Time.deltaTime;
 
-        //時間を表示する
-        timeText.text = countdown.ToString("f2");
-
+            //時間を表示する
+            timeText.text = countdown.ToString("f2");
+        }
         //countdownが0以下になったとき
         if (countdown <= 0)
         {
