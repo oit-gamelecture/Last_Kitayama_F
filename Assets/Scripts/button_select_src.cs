f54b368b29c97cfd1e8acbfd951ce01c2ce42aba
0yu-uy0 <e1c24059@oit.ac.jp>
2024-10-29 19:02:35 +0900

    10/29 19:02 中井優心 タイトル画面のセレクトに矢印追加。マウスクリックすると止まる

---
+++ Assets/Scripts/button_select_src.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class button_select_src : MonoBehaviour
+{
+    [SerializeField] EventSystem eventSystem;
+    bool select_f = false;                      //ボタンが選択されるとtrue
+    public GameObject yajirusi;                 //矢印のイメージをセット
+    GameObject selectedObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (select_f == false)
+        {
+            try
+            {
+                selectedObj = eventSystem.currentSelectedGameObject.gameObject;
+                if (this.gameObject == selectedObj)    　//ボタンが選択されていたら
+                {
+                    yajirusi.SetActive(true);           //矢印を表示
+                    select_f = true;
+                }
+            }
+            catch
+            {
+                //選択されない場合は何もしない
+            }
+        }
+        else
+        {
+            try
+            {
+                selectedObj = eventSystem.currentSelectedGameObject.gameObject;
+                if(this.gameObject != eventSystem.currentSelectedGameObject.gameObject) //選択から外れたら
+                {
+                    yajirusi.SetActive(false);  //矢印を非表示
+                    select_f = false;
+                }
+            }
+            catch
+            {
+                //選択されない場合は何もしない
+            }
+        }
+    }
+}
