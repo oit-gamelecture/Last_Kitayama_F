273d81b13c9dd39997fde2d4e3ae76e6b2e4f454
辻響 <0131Hibiki-0131@users.noreply.github.com>
2024-11-02 17:59:27 +0900

    11/2 17:59 辻響　音量調節を作った。

--- Assets/Scripts/VolumeControl.cs
+++ Assets/Scripts/VolumeControl.cs
@@ -1,21 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class VolumeControl : MonoBehaviour
 {
-    [SerializeField] private Slider volumeSlider;
+    [SerializeField] private Button volumeButton;      // 音量調節ボタン
+    [SerializeField] private Slider volumeSlider;      // 音量スライダー
 
     private void Start()
     {
+        // 初期設定
         volumeSlider.value = AudioListener.volume;
 
-        volumeSlider.onValueChanged.AddListener(SetVolume);
+        volumeButton.Select(); // 初期選択を音量調節ボタンに
+        volumeSlider.gameObject.SetActive(true); // スライダーを常に表示
     }
 
-    private void SetVolume(float volume)
+    private void Update()
     {
-        AudioListener.volume = volume;
+        // EscキーでTitleシーンに遷移
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Title");
+        }
+
+        // AキーとDキーでスライダーの値を調整
+        if (Input.GetKey(KeyCode.D))
+        {
+            volumeSlider.value = Mathf.Clamp(volumeSlider.value + 0.005f, 0, 1);
+            AudioListener.volume = volumeSlider.value; // 音量を更新
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            volumeSlider.value = Mathf.Clamp(volumeSlider.value - 0.005f, 0, 1);
+            AudioListener.volume = volumeSlider.value; // 音量を更新
+        }
+
+        // Enterキーでボタン選択に戻る
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            volumeButton.Select(); // 音量ボタンにカーソルを戻す
+        }
     }
 }
--- Assets/Scripts/button_select_src.cs
+++ Assets/Scripts/button_select_src.cs
@@ -10,15 +10,11 @@ public class button_select_src : MonoBehaviour
     bool select_f = false;                      //ボタンが選択されるとtrue
     public GameObject yajirusi;                 //矢印のイメージをセット
     GameObject selectedObj;
-
-    public AudioClip soundEffect;      // 効果音のオーディオクリップ
-    private AudioSource audioSource;   // 効果音再生用のAudioSource
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
-        audioSource = gameObject.AddComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -52,9 +48,7 @@ public class button_select_src : MonoBehaviour
                 selectedObj = eventSystem.currentSelectedGameObject.gameObject;
                 if (this.gameObject != eventSystem.currentSelectedGameObject.gameObject) //選択から外れたら
                 {
-                    yajirusi.SetActive(false);//矢印を非表示
-                    audioSource.clip = soundEffect;
-                    audioSource.Play();
+                    yajirusi.SetActive(false);  //矢印を非表示
                     select_f = false;
                 }
             }
