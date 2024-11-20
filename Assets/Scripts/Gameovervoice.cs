976f655b583b58dadefaface63766c8975600ed6
edaseigo <e1b24014@oit.ac.jp>
2024-11-20 18:22:55 +0900

    11/20 18:21 江田正剛　ゲームオーバーでランダムにボイスが流れるようにした

---
+++ Assets/Scripts/Gameovervoice.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameovervoice : MonoBehaviour
+{
+    public AudioClip sound1; // 最初の効果音
+    public List<AudioClip> soundClips;
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        StartCoroutine(PlaySoundsInSequence());
+    }
+
+
+    private IEnumerator PlaySoundsInSequence()
+    {
+        // 最初の効果音を再生
+        audioSource.clip = sound1;
+        audioSource.Play();
+        // 再生終了まで待機
+        yield return new WaitForSeconds(audioSource.clip.length);
+
+
+        // ランダムなインデックスを取得
+        int randomIndex = Random.Range(0, soundClips.Count);
+
+        // ランダムに選ばれた効果音を再生
+        audioSource.PlayOneShot(soundClips[randomIndex]);
+    }
+
+}
