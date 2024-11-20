1db434b4ff61be5a55a9e862555e23ab16451dc0
辻響 <0131Hibiki-0131@users.noreply.github.com>
2024-11-20 19:14:32 +0900

    11/20 19:14 辻響　表示×非表示

---
+++ Assets/Scripts/ReportManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReportManager : MonoBehaviour
+{
+    // 非表示にしたいUIオブジェクト
+    public GameObject uiElement;
+
+    // 衝突したプレイヤー
+    private void OnTriggerEnter(Collider other)
+    {
+        // プレイヤーと衝突した場合
+        if (other.CompareTag("Player"))
+        {
+            // UIを表示
+            uiElement.SetActive(true);
+
+            // 3秒後にUIを非表示にするコルーチンを開始
+            StartCoroutine(HideUIAfterDelay(3f));
+        }
+    }
+
+    // UIを指定した時間後に非表示にするコルーチン
+    private IEnumerator HideUIAfterDelay(float delay)
+    {
+        // 指定時間待機
+        yield return new WaitForSeconds(delay);
+
+        // UIを非表示
+        uiElement.SetActive(false);
+    }
+}
+
+
