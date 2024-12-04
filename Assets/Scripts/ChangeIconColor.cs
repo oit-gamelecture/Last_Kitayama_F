fa9cccdefad9c42cc96e7682ba517b3c14531828
0yu-uy0 <e1c24059@oit.ac.jp>
2024-12-04 16:27:55 +0900

    12/4 16:27 中井優心 UIもろもろ変えた

--- Assets/Scripts/ChangeIconColor.cs
+++ Assets/Scripts/ChangeIconColor.cs
@@ -3,33 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ChangeIconColor : MonoBehaviour
+public class ChangeOutlineColor : MonoBehaviour
 {
-    public Image targetIcon; // 色を変更するアイコンを割り当てる
+    public Outline targetOutline; // 色を変更するアウトラインを割り当てる
     private Color originalColor;
-    // Start is called before the first frame update
+
     void Start()
     {
-        if (targetIcon != null)
+        if (targetOutline != null)
         {
-            originalColor = targetIcon.color;
+            originalColor = targetOutline.effectColor; // 元のアウトライン色を保存
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (targetIcon != null)
+        if (targetOutline != null)
         {
             // Fキーが押された時に赤くする
             if (Input.GetKey(KeyCode.F) || Input.GetKeyDown("joystick button 0"))
             {
-                targetIcon.color = Color.red;
+                targetOutline.effectColor = Color.red;
             }
             // Fキーを離した時に元の色に戻す
-            else if(Input.GetKeyUp(KeyCode.F) || Input.GetKeyUp("joystick button 0"))
+            else if (Input.GetKeyUp(KeyCode.F) || Input.GetKeyUp("joystick button 0"))
             {
-                targetIcon.color = originalColor;
+                targetOutline.effectColor = originalColor;
             }
         }
     }
